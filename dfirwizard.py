2eec72951cd139e9aa49bc1758db0897cba29bb0
JaFizz <s1096525@student.hsleiden.nl>
2017-06-21 13:58:41 +0200

    Pytsk Partitietable, inodenummer, metadata, creation date, extract werkend

--- dfirwizard.py
+++ dfirwizard.py
@@ -7,7 +7,7 @@ import datetime
 
 
 #imagebestand
-imagefile = "Windows10.E01"
+imagefile = "/media/sf_sf/Windows10.E01"
 
 #image handle
 imagehandle = pytsk3.Img_Info(imagefile)
@@ -21,11 +21,31 @@ for partition in partitionTable:
 
 filesystemObject = pytsk3.FS_Info(imagehandle, offset=1048576)
 
-fileobject = filesystemObject.open("/$MFT")
+fileobject = filesystemObject.open("/Recovery/WindowsRE/boot.sdi")
 print "\nFile Inode:",fileobject.info.meta.addr
 print "File Name:",fileobject.info.name.name
-print "File Creation Time:",datetime.datetime.fromtimestamp(fileobject.info.meta.crtime).strftime('%Y-%m-%d %H:%M:%S')
+print "File Creation Time:",datetime.datetime.fromtimestamp(fileobject.info.meta.crtime).strftime('%Y-%m-%d %H:%M:%S')+"\n"
 
 outfile = open('DFIRWizard-ouput', 'w')
 filedata = fileobject.read_random(0,fileobject.info.meta.size)
 outfile.write(filedata)
+
+path = "/Boot" #verander naar / voor root directory
+inode = 160 #verander naar 0 voor root directory
+
+directory = filesystemObject.open_dir(path=path, inode=inode)
+
+print 'Directories:'
+
+for f in directory:
+    print f.info.meta.size, f.info.name.name
+
+outfile2 = open ('boot.sdi','w')
+fd = fileobject.read_random(0,fileobject.info.meta.size)
+outfile2.write(fd)
+
+f = filesystemObject.open_meta(inode = 174)
+
+print '\n'
+
+print f.info.meta.type
