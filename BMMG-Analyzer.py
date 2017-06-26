c751dad61ee081b92a0b12e35089409dc30ee41c
JaFizz <s1096525@student.hsleiden.nl>
2017-06-26 20:00:34 +0200

    Werkmap, Casusmap, Savefilemap, foutafvanging

--- BMMG-Analyzer.py
+++ BMMG-Analyzer.py
@@ -13,10 +13,21 @@ import os
 d = datetime.datetime.now()
 datum = "%s/%s/%s" %(d.day, d.month, d.year)
 tijd = time.strftime("%X")
+werkMap = "BMMG_Werkmap"
+
+saveFileMap = "/savefile/"
+extractsMap = "/extracts/"
 
 #SQL
 kolomCasusNaam = 'casus_naam'
 kolomOnderzoekerNaam = 'onderzoeker_naam'
+kolomCasusMap = 'locatie_casusmap'
+
+tabelImageBestand = "ImageBestand"
+kolomImageNaam = "image_naam"
+kolomImageLocatie = "image_locatie"
+
+
 #kolomDataBLOB = 'data_blob'
 #intDataType = 'INTEGER'
 #floatDataType = 'REAL'
@@ -26,19 +37,51 @@ textDataType = 'TEXT'
 
 #FUNCTIE nieuwe casus
 def nieuweCasusToevoegen():
-
     print("\nEen nieuwe casus toevoegen")
 
-    #casusnaam invoeren - foutafvanging moet nog ingevoerd worden
-    casusNaam = input("Casus Naam: ").replace(" ", "_")
-    if(casusNaam == ""):
+    #casusnaam invoeren - foutafvanging moet nog
+    casusNaam = raw_input(str("Casus Naam: "))
+    if not casusNaam:
         casusNaam = ("BMMG_Casus_" + str(randint(0,99999999)))
         print("Casus Naam: " + casusNaam)
+    casusNaam = casusNaam.replace(" ", "_")
 
-    onderzoekerNaam = str(input("Onderzoeker Naam: "))
+    #onderzoekernaam invoeren
+    onderzoekerNaam = str(raw_input("Onderzoeker Naam: "))
+    if not onderzoekerNaam:
+        onderzoekerNaam = "Unknown"
+        print("Onderzoeker Naam: "+onderzoekerNaam+"\n")
 
-    #databasebestand
-    databaseBestand = casusNaam + ".BMMG"
+    #opslaglocatie laten kiezen
+    opslagLocatie = filedialog.askdirectory(title='Geef aan in welke map u de casusbestanden wilt opslaan.'
+                                                  '\nIn deze map worden de savefile en extracties opgeslagen')
+    if not opslagLocatie:
+        try:
+            opslagLocatie = filedialog.askdirectory(title='Geef aan in welke map u de casusbestanden wilt opslaan.'
+                                                          '\nIn deze map worden de savefile en extracties opgeslagen\n'
+                                                          'Kies nu een map, anders wordt het programma afgesloten!')
+        except:
+            print "Twee keer geen opslaglocatie gekozen, het programma wordt afgesloten"
+            time.sleep(5)
+            sys.exit()
+
+    ###mappen aanmaken
+    #werkmap aanmaken in de opslaglocatie
+    werkMapLocatie = (opslagLocatie+"/"+werkMap)
+    if not os.path.exists(werkMapLocatie):
+        os.makedirs(werkMapLocatie)
+    #casusmap aanmaken in de werkmap
+    casusMap = werkMapLocatie + "/" + casusNaam
+    if not os.path.exists(casusMap):
+        os.makedirs(casusMap)
+    #savefilemap aanmaken in casusmap
+    if not os.path.exists(casusMap + saveFileMap):
+        os.makedirs(casusMap + saveFileMap)
+
+    databaseBestandNaam = casusNaam + ".BMMG"
+
+    #databasebestand maken in de savefilemap van de casusmap
+    databaseBestand = casusMap + saveFileMap + databaseBestandNaam
 
     #connectie maken naar SQLite database
     connectie = sqlite3.connect(databaseBestand)
@@ -46,48 +89,70 @@ def nieuweCasusToevoegen():
     #cursor object aanmaken
     c = connectie.cursor()
 
+    #casusNaamInfo variable maken voor de database
     casusNaamInfo = casusNaam +"_info"
 
     #nieuwe tabel maken met casusnaam kolom
-    c.execute('CREATE TABLE {tabelnaam} ({kolomnaam} {datatype})'\
-              .format(tabelnaam=casusNaamInfo, kolomnaam=kolomCasusNaam, datatype=textDataType))
+    c.execute('CREATE TABLE {tabelnaam} ({kolomnaam} {datatype})'.format(tabelnaam=casusNaamInfo,
+                                                                         kolomnaam=kolomCasusNaam, datatype=textDataType))
 
     #niewe kolommen toevoegen
-    c.execute("ALTER TABLE {tabelnaam} ADD COLUMN '{kolomnaam}' {datatype}"\
+    c.execute("ALTER TABLE {tabelnaam} ADD COLUMN '{kolomnaam}' {datatype}"
               .format(tabelnaam=casusNaamInfo, kolomnaam=kolomOnderzoekerNaam, datatype=textDataType))
+    c.execute("ALTER TABLE {tabelnaam} ADD COLUMN '{kolomnaam}' {datatype}"
+              .format(tabelnaam=casusNaamInfo, kolomnaam=kolomCasusMap, datatype=textDataType))
 
     #database vullen
-    c.execute("INSERT INTO {tabelnaam} ({kolomnaam1}, {kolomnaam2}) VALUES ('{value1}', '{value2}')"\
-              .format(tabelnaam=casusNaamInfo, kolomnaam1=kolomCasusNaam, kolomnaam2=kolomOnderzoekerNaam, value1=casusNaam, value2=onderzoekerNaam))
+    c.execute("INSERT INTO {tabelnaam} ({kolomnaam1}, {kolomnaam2}, {kolomnaam3}) VALUES ('{value1}', '{value2}', '{value3}')"
+              .format(tabelnaam=casusNaamInfo, kolomnaam1=kolomCasusNaam, kolomnaam2=kolomOnderzoekerNaam,
+                      kolomnaam3=kolomCasusMap, value1=casusNaam, value2=onderzoekerNaam, value3=casusMap))
 
     #commit
     connectie.commit()
 
-    print("Welkom " + onderzoekerNaam + ". De casus is aangemaakt. De savefile: '" + databaseBestand + "' is aangemaakt.")
+    if onderzoekerNaam == "Unknown":
+        print("Welkom! De casus is aangemaakt.\n"
+              "De savefile: '" + databaseBestandNaam + "' is aangemaakt.\n"
+                                             "De savefile is te vinden in de map: "+casusMap+saveFileMap+"\n"
+                                                                                             "Met behulp van de savefile kunt u verdergaan met de casus wanneer u wilt.\n")
+    else:
+        print("Welkom " + onderzoekerNaam + "! De casus is aangemaakt.\n"
+                                            "De savefile: '" + databaseBestandNaam + "' is aangemaakt.\n"
+                                                                           "De savefile is te vinden in de map: "+casusMap+saveFileMap+"\n"
+                                                                           "Met behulp van de savefile kunt u verdergaan met de casus wanneer u wilt.\n")
 
     #imagebestand openen
     print("Geef image-bestand op:")
-    imageBestand = filedialog.askopenfilename(filetypes=[(".img, .raw, .E01, etc..", ["*.img","*.raw","*.txt","*.mp3"],)], title='Geef image-bestand op:')
-
+    #imageBestand = filedialog.askopenfilename(filetypes=[(".img, .raw, .E01, etc.."), ["*.img","*.raw","*.E01","*.e01","*.dd"]], title='Geef image-bestand op:')
+    imageBestand = filedialog.askopenfilename(initialdir = "/",title = "Geef image-bestand op:",filetypes = (("E01","*.E01"),("all files","*.*")))
     if not imageBestand:
-        print("Geen image-bestand geselecteerd, het programma wordt afgesloten")
-        time.sleep(5)
-        sys.exit()
-
+        try:
+            imageBestand = filedialog.askopenfilename(initialdir = "/",title = "Geef image-bestand op:",filetypes = (("E01","*.E01"),("all files","*.*")))
+        except:
+            print("Twee keer geen image-bestand geselecteerd, het programma wordt afgesloten")
+            time.sleep(5)
+            sys.exit()
+    #imageName genereren
     imageName1 = os.path.splitext(os.path.basename(imageBestand))[0]
     extensie = os.path.splitext(imageBestand)[1]
     imageName = imageName1 + extensie
     print("\n"+imageName + " is toegevoegd aan de casus.")
 
     #ImageBestand tabel maken
-    c.execute("CREATE TABLE ImageBestand (image_naam TEXT NOT NULL PRIMARY KEY, image_bestand BLOB NOT NULL);")
+    c.execute('CREATE TABLE {tabelnaam} ({kolomnaam} {datatype})'.format(tabelnaam=tabelImageBestand,
+                                                                         kolomnaam=kolomImageNaam, datatype=textDataType))
 
-    imageBestandOpen = open(imageBestand, 'rb').read()
+    #ImageBestand kolom toevoegen
+    c.execute("ALTER TABLE {tabelnaam} ADD COLUMN '{kolomnaam}' {datatype}"
+              .format(tabelnaam=tabelImageBestand, kolomnaam=kolomImageLocatie, datatype=textDataType))
 
-    c.execute("INSERT INTO ImageBestand (image_naam, image_bestand) values (?, ?)",
-              (imageName, sqlite3.Binary(imageBestandOpen)))
+    #ImageBestand tabel vullen
+    c.execute("INSERT INTO {tabelnaam} ({kolomnaam1},{kolomnaam2}) VALUES ('{value1}','{value2}')"
+              .format(tabelnaam=tabelImageBestand, kolomnaam1=kolomImageNaam, kolomnaam2=kolomImageLocatie, value1=imageName, value2=imageBestand))
+    #commit
     connectie.commit()
 
+    #Naar werkMenu()
     werkMenu(casusNaam, connectie, c)
 
 #FUNCTIE bestaande casus
@@ -95,18 +160,26 @@ def bestaandeCasusOpenen():
     print("\nOpen een bestaande casus")
 
     #databasebestand
-    databaseBestand = filedialog.askopenfilename(filetypes=[(".BMMG", ["*.BMMG"],)], title='Open een bestaande casus:')
+    #databaseBestand = filedialog.askopenfilename(filetypes=[(".BMMG", ["*.BMMG"],)], title='Open een bestaande casus:')
+    databaseBestand = filedialog.askopenfilename(initialdir = "/",title = "Open een bestaande casus:",filetypes = (("BMMG-File","*.BMMG"),))
 
-    #bestandsextensie verwijderen van de bestandsnaam
+    if not databaseBestand:
+        try:
+            databaseBestand = filedialog.askopenfilename(initialdir = "/",title = "Open een bestaande casus:",filetypes = (("BMMG-File","*.BMMG"),))
+        except:
+            print "Twee keer geen savefile opgegeven, het programma wordt afgesloten"
+            time.sleep(5)
+            sys.exit()
+
+    #bestandsextensie verwijderen van de bestandsnaam om zo casusnaaminfo te maken die nodig is in de database
     casusNaam = os.path.basename(databaseBestand[:-5])
+    casusNaamInfo = casusNaam +"_info"
 
     #connectie maken naar SQLite database
     connectie = sqlite3.connect(databaseBestand)
 
     #cursor object aanmaken
     c = connectie.cursor()
-
-    casusNaamInfo = casusNaam +"_info"
 
     #data opvragen uit de database en printen op scherm
     c.execute('SELECT * FROM {tabelnaam}'. \
@@ -117,6 +190,7 @@ def bestaandeCasusOpenen():
     #commit
     connectie.commit()
 
+    #Naar het werkmenu
     werkMenu(casusNaam, connectie, c)
 
 def werkMenu(casusNaam, connectie, c):
@@ -124,48 +198,38 @@ def werkMenu(casusNaam, connectie, c):
     print("\nWerkmenu:\n")
 
     #Werkmenu opties
-    print("Optie 1: Casusinformatie opvragen")
-    print("Optie 2: Bestand wegschrijven")
+    print("Optie 1: Casusnaam opvragen")
+    print("Optie 2: Leeg")
     print("Optie 3: BMMG-Analyzer afsluiten")
 
-    try:
-        optie = int(input("\nKies een optie: "))
-    except ValueError:
-        sys.exit("Geen optie opgegeven")
+    optie = int(raw_input("\nKies een optie: "))
+    if not optie:
+        try:
+            optie = int(raw_input("\nKies een optie. Vul een cijfer in!!!: "))
+        except ValueError:
+            sys.exit("Twee keer geen optie opgegeven, het programma wordt afgesloten")
 
+    #casusnaaminfo maken voor de database
     casusNaamInfo = casusNaam+"_info"
 
     if(optie == 1):
-    #casusinformatie opvragen uit de database en printen op scherm
+    #casusnaam opvragen uit de database en printen op scherm
         c.execute('SELECT casus_naam FROM {tabelnaam}'. \
               format(tabelnaam=casusNaamInfo))
         dataUitDB = c.fetchone()
         print("\nCasus Naam: "+(dataUitDB[0]))
 
+        #weer naar werkmenu
         werkMenu(casusNaam, connectie, c)
 
     if(optie == 2):
-        c.execute('SELECT image_naam FROM ImageBestand')
-        imageNaam = c.fetchone()[0]
-
-        #opslaglocatie
-        print("Waar wilt u het imagebestand opslaan?")
-        opslagLocatie = filedialog.askdirectory(title='Geef aan waar u het imagebestand wilt opslaan:')
-
-        with open(opslagLocatie+"\\"+imageNaam, 'wb') as imageBestand:
-            c.execute('SELECT image_bestand FROM ImageBestand')
-            ablob = c.fetchone()
-            imageBestand.write(ablob[0])
-
-    #    try:
-        print("\nImagebestand is weggeschreven")
-    #    except IOError:
-    #        print("Geen imagebestand weggeschreven")
 
         werkMenu(casusNaam, connectie, c)
 
     if(optie == 3):
+        print "Het programma wordt over 3 seconden afgesloten..."
         connectie.close()
+        time.sleep(3)
         sys.exit()
 
 #MAIN CODE
@@ -185,11 +249,17 @@ print("Goedendag, wat wilt u doen? \n")
 print("Optie 1: Een nieuwe casus toevoegen")
 print("Optie 2: Ga verder met een bestaande casus \n")
 
-try:
-    optie = int(input("Kies een optie: "))
-except ValueError:
-    sys.exit("Geen optie opgegeven")
+#optie kiezen
+optie = int(raw_input("Kies een optie: "))
+if not optie:
+    try:
+        optie = int(raw_input("Kies een optie. Vul een cijfer in!!! "))
+    except:
+        print("Twee keer geen optie opgegeven, het programma wordt afgesloten.")
+        time.sleep(5)
+        sys.exit()
 
+#doorsturen naar gekozen optie
 if(optie == 1):
     nieuweCasusToevoegen()
 
--- README.txt
+++ README.txt
@@ -1,15 +1,36 @@
 ===HELP GUIDE===
-
+#Stap-Voor-Stap
 Stap 1: Start het script
-Stap 2: Kies een optie uit het menu
+Stap 2: Kies een optie uit het menu (zie #Opties)
 
-Optie 1: Een nieuwe casus toevoegen
-Optie 2: Ga verder met een bestaande casus
+#Werkmap
+Bij het aanmaken van een nieuwe casus kiest u een locatie.
 
+#Casusmap
+In deze map wordt een casusmap gegenereerd waarin de savefile en extracties worden opgeslagen.
+
+#Savefile
 Wanneer er een nieuwe casus aangemaakt wordt, zal er een savefile gegenereerd worden.
 Deze savefile heeft de extensie: '.BMMG'. Met dit bestand kunt u op elk gewenst moment verdergaan met een casus.
+De savefile is terug te vinden in de casusmap in het mapje '/savefile'.
 
+#SQLite database
+De savefile is een SQLite database
+
+#Werkmenu
 Als u een nieuwe casus heeft aangemaakt of een bestaande casus opent, dan komt u in het werkmenu terecht.
 Het werkmenu heeft de volgende opties:
 
-Optie 1:
+#Opties
+Optie 1: Casusnaam opvragen
+Optie 2: 
+Optie 3: BMMG-Analyzer afsluiten
+
+#Optie 1:
+Casusnaam wordt opgevraagd uit de database en geprint op het scherm
+
+#Optie 2:
+
+
+#Optie 3:
+BMMG-Analyzer wordt afgesloten
